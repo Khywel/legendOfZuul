e484c931af813539eb33fa91c7994c54e9db59ad
Sixtine12 <34026947+Sixtine12@users.noreply.github.com>
2018-11-14 17:42:18 +0100

    Add files via upload

---
+++ Boss.java
@@ -0,0 +1,28 @@
+/**
+ *  This class allows to create NPC.
+ *  A NPC is a neutral character in the game.
+ *  He doesn't attack the player.
+ *  He can't die. He is able to give quests to the player.
+ *
+ *
+ * @author  Sixtine Vernhes
+ * @version 14/11/18
+ */
+
+public class Boss extends Enemy
+{
+    private List<Quest> quetes;
+    /**Creates a Boss who is an actor with an empty quest list at the beginning
+	* The quest list will be filled in the constructor with the main quest : get the 6 statuette back
+	*/
+    public NPC()
+    {
+        super();
+    }
+
+    /**Add a new quest in the Quest list of the player
+    return true if the quest is added*/
+    public boolean giveQuestToNPC(Quest quest)
+    {
+    }
+}
